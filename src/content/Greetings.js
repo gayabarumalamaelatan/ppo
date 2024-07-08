b3c394ba338a67ea9d10613864256ea5abe7e3e6
gayabarumalamselatan <soerjahartono22@gmail.com>
2024-07-08 14:26:49 +0700

    Edit tipis

--- src/content/Greetings.js
+++ src/content/Greetings.js
@@ -5,7 +5,7 @@ const Greetings = () => {
   const greeting = currentHour < 12 ? "Good Morning!" : "Good Evening!";
 
   return (
-    <h2 style={{fontWeight: "600"}}>{greeting}</h2>
+    <h2 style={{fontWeight: "700"}}>{greeting}</h2>
   )
 }
 
