4e6cb04c0e8c8d4a07886e0897d42db8ae69246c
gayabarumalamselatan <soerjahartono22@gmail.com>
2024-08-13 16:28:13 +0700

    nambah baru

--- src/content/Greetings.js
+++ src/content/Greetings.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Greetings = () => {
   const currentHour = new Date().getHours();
-  const greeting = currentHour < 12 ? "Good Morning!" : "Good Evening!";
+  let greeting;
+  // const greeting = currentHour < 12 ? "Good Morning!" : "Good Evening!";
+  if (currentHour < 12) {
+    greeting = "Good Morning!";
+  } else if (currentHour < 18) {
+    greeting = "Good Afternoon!";
+  } else {
+    greeting = "Good Evening!";
+  }
 
   return (
     <h2 style={{fontWeight: "700"}}>{greeting}</h2>
